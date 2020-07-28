b5671cc9782de86354368941ee98218d3f54f497
77rtoip <44272828+77rtoip@users.noreply.github.com>
2020-07-28 15:05:04 +0200

    Update crypto.h

--- crypto.h
+++ crypto.h
@@ -8,6 +8,6 @@ std::vector<char> charwiseXor(const std::vector<char> &, const char &);
 std::vector<char> frequencyVector(const std::vector<char> &);
 int frequencyScoring(const std::vector<char> &);
 std::vector<char> hex_string_to_char_vector(const char*);
-std::string vector_to_string(const std::vector<char> &);
+std::string to_string(const std::vector<char> &);
 
 #endif // CRYPTO_H_INCLUDED
